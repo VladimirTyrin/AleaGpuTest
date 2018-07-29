ca4a2b874dffc80e25c228b6ebba5fac2229ec23
Vladimir Tyrin <b0-0b@yandex.ru>
2018-07-30 01:11:47 +0300

    Fix

--- AleaGpuTest/Program.cs
+++ AleaGpuTest/Program.cs
@@ -4,8 +4,6 @@ namespace AleaGpuTest
 {
     internal static class Program
     {
-        private const int ArraySize = 128 * 1024 * 1024;
-
         private static void Main(string[] args)
         {
             BenchmarkRunner.Run<AverageComputationBenchmark>();
