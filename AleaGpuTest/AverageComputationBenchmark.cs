57f49eae7fe63b50cb604c00f3adb66ab0449ef1
Vladimir Tyrin <b0-0b@yandex.ru>
2018-07-30 01:52:17 +0300

    Some new methods

--- AleaGpuTest/AverageComputationBenchmark.cs
+++ AleaGpuTest/AverageComputationBenchmark.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Numerics;
 using System.Threading.Tasks;
 using Alea;
 using Alea.Parallel;
@@ -47,10 +48,53 @@ namespace AleaGpuTest
         }
 
         [Benchmark]
+        public double VectorizedAverage()
+        {
+            return VectorRangeSum(_data, 0, DataSize) / DataSize;
+        }
+
+        [Benchmark]
+        public double ParallelVectorizedAverage()
+        {
+            var parts = Environment.ProcessorCount;
+            var partSize = DataSize / parts;
+            var averages = new double[parts];
+
+            Parallel.For(0, parts, part =>
+            {
+                var lowerBound = partSize * part;
+                var upperBound = lowerBound + partSize;
+                averages[part] = VectorRangeSum(_data, lowerBound, upperBound) / partSize;
+            });
+
+            return averages.Average();
+        }
+
+        [Benchmark]
         public double GpuAverage()
         {
             var gpu = Gpu.Default;
             return gpu.Average(_data);
+        }
+
+        private static double VectorRangeSum(float[] array, int lowerInclusive, int upperExclusize)
+        {
+            var count = upperExclusize - lowerInclusive;
+            var vectorSize = Vector<float>.Count;
+            var vectorCount = count / vectorSize;
+            var sum = new Vector<float>();
+            for (var i = 0; i < vectorCount; ++i)
+            {
+                sum += new Vector<float>(array, lowerInclusive + i * vectorSize);
+            }
+
+            var totalSum = 0d;
+            for (var i = 0; i < vectorSize; ++i)
+            {
+                totalSum += sum[i];
+            }
+
+            return totalSum;
         }
 
         private static double RangeSum(float[] array, int lowerInclusive, int upperExclusize)
---
+++ AleaGpuTest/FindPrimesBenchmark.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BenchmarkDotNet.Attributes;
+
+namespace AleaGpuTest
+{
+    public class FindPrimesBenchmark
+    {
+        private const int PrimesToFind = 100_000;
+
+        [Benchmark]
+        public int[] FindPrimesLinq()
+        {
+            return NaturalSequence().Where(IsPrime).Take(PrimesToFind).ToArray();
+        }
+
+        [Benchmark]
+        public int[] FindPrimesCpuBlockParallel()
+        {
+            var cpuCount = Environment.ProcessorCount;
+            const int blockSize = 128 * 1024;
+            var buffers = new List<int>[cpuCount];
+            var result = new List<int>();
+
+            void InCpuRange(Action<int> action)
+            {
+                for (var i = 0; i < cpuCount; ++i)
+                {
+                    action(i);
+                }
+            }
+
+            InCpuRange(i => buffers[i] = new List<int>());
+            
+            var offset = 0;
+
+            while (result.Count < PrimesToFind)
+            {
+                InCpuRange(i => buffers[i].Clear());
+
+                var offsetCopy = offset;
+                Parallel.For(0, cpuCount, cpu =>
+                {
+                    var lowerInclusive = offsetCopy + (cpu * blockSize);
+
+                    buffers[cpu].AddRange(Enumerable.Range(lowerInclusive, blockSize).Where(IsPrime));
+                });
+
+                InCpuRange(i => result.AddRange(buffers[i]));
+                offset += cpuCount * blockSize;
+            }
+
+            return result.Take(PrimesToFind).ToArray();
+        }
+
+        private static IEnumerable<int> NaturalSequence()
+        {
+            var current = 1;
+            while (true)
+            {
+                yield return current;
+                ++current;
+            }
+        }
+
+        private static bool IsPrime(int number)
+        {
+            var sqrt = (int) Math.Sqrt(number);
+
+            for (var i = 2; i <= sqrt; ++i)
+            {
+                if (number % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+    }
+}
